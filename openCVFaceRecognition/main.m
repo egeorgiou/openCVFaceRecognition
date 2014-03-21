c6c1304319f646db4450b631dddea12b793a0d63
Evangelos Georgiou <evangelos.georgiou@kcl.ac.uk>
2014-03-21 12:39:26 +0000

    Initial Commit

---
+++ openCVFaceRecognition/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  openCVFaceRecognition
+//
+//  Created by Evangelos Georgiou on 21/03/2014.
+//  Copyright (c) 2014 Evangelos Georgiou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ openCVFaceRecognitionTests/openCVFaceRecognitionTests.m
@@ -0,0 +1,34 @@
+//
+//  openCVFaceRecognitionTests.m
+//  openCVFaceRecognitionTests
+//
+//  Created by Evangelos Georgiou on 21/03/2014.
+//  Copyright (c) 2014 Evangelos Georgiou. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface openCVFaceRecognitionTests : XCTestCase
+
+@end
+
+@implementation openCVFaceRecognitionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
